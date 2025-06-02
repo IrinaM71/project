677535515a6ff10b8227bb90d598b0917155f3a9
IrinaM71 <irsola71@gmail.com>
2025-06-02 10:15:39 +0200

    Created a repositoriy for the project

---
+++ index.js
@@ -0,0 +1,28 @@
+//class User {
+//constructor(name) {
+//this.name = name;
+//}
+//register() {
+//console.log("register...");
+//}
+
+//static isUser() {
+//console.log("checking if user");
+//}
+//}
+
+//const user1 = new User("Alis");
+
+//console.log(user1.name);
+//user1.register();
+class Calculator {
+  static add(a, b) {
+    console.log(a + b);
+  }
+}
+
+//class StringUtuls {
+//static reverse(str) {
+//return str.split("").reverse().join("");
+//}
+//}
